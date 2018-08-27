07be7129fe5906a34c50dd763ffd8d891f2098c3
Richard <ricstr@south.melville>
2018-08-27 22:38:14 +0100

    Object Structure for City

---
+++ index.js
@@ -0,0 +1,7 @@
+function City(capital, continent, country, name, pictures) {
+    this.capital = capital,
+    this.continent = continent,
+    this.country = country,
+    this.name = name,
+    this.pictures = pictures
+}
