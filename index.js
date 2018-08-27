9b709baea8f58e5d244ec860f7765dfdd2b2a9ac
Richard <ricstr@south.melville>
2018-08-27 22:42:53 +0100

    Added comments

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function City(capital, continent, country, name, pictures) {
-    this.capital = capital,
-    this.continent = continent,
-    this.country = country,
-    this.name = name,
-    this.pictures = pictures
+    this.capital = capital, // true or false
+    this.continent = continent, // string
+    this.country = country, // string
+    this.name = name, // string
+    this.pictures = pictures // array of 5 url's
 }
